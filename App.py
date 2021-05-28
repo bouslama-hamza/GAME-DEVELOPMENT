02752744c13d7c66377735b46a738417c1b43d64
bouslama-hamza <ham.bousa98@gmail.com>
2021-05-28 08:12:08 +0100

    About page

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app= Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testDB'
 db = SQLAlchemy(app)
-
+'''
 class Student(db.Model): 
 
     id = db.Column(db.Integer, primary_key = True)
@@ -15,8 +15,8 @@ class Student(db.Model):
     player2 = db.Column(db.String(100), nullable= False)
     def __repr__(self):
         return f"Student('{self.name}','{self.email}','{self.player1}','{self.player2}')"
-usera=[]
-
+usera=[]'''
+ 
 @app.route('/')
 def index():
     return render_template("Sign.html")
@@ -31,12 +31,13 @@ def players():
 @app.route('/game', methods=['GET','POST'])
 def game():
 
-    global user1
-    user1=Student(name=usera[0],email=usera[1],player1=request.form['player1'],player2=request.form['player2'])
-    db.session.add(user1)
-    db.session.commit()
-    print(Student.query.all())
     return render_template('retro.html')
+
+
+@app.route('/About', methods=['GET','POST'])
+def how():
+
+    return render_template('how.html')
 
 if (__name__=="__main__"):
     app.run(debug=True,port=8456)
